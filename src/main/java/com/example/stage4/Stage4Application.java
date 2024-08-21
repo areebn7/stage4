011cbcb863be9666f47f4f022be92e9427077db8
Areeb Nabi <anabi001@odu.edu>
2024-08-21 10:59:05 -0400

    stage4 first commit

---
+++ src/main/java/com/example/stage4/Stage4Application.java
@@ -0,0 +1,13 @@
+package com.example.stage4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Stage4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Stage4Application.class, args);
+	}
+
+}
