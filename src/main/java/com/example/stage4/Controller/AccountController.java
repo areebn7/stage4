97169a133e66aef55eae39bb0157494366351148
Areeb Nabi <anabi001@odu.edu>
2024-08-21 14:22:28 -0400

    implement token generation using post method

--- src/main/java/com/example/stage4/Controller/AccountController.java
+++ src/main/java/com/example/stage4/Controller/AccountController.java
@@ -1,21 +1,30 @@
 package com.example.stage4.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
 
-import com.example.stage4.JwtUtil;
 import com.example.stage4.Model.Account;
+import com.example.stage4.Security.TokenService;
 
 @RestController
-@RequestMapping("/account")
 public class AccountController {
 
-    @GetMapping
-    public String homePage() {
-        return "Welcome to my REST API!";
+      @Autowired
+    TokenService tokenService;
+
+    @PostMapping("/token")
+    public String token(Authentication authentication) {
+        return tokenService.generateToken(authentication);
+    }
+
+    @GetMapping("/")
+    public String health() {
+        return "Authorization Server is running.";
     }
 }
