b6c5ac8a56bbc9a5f9d6a2bca649aac61005eb37
Areeb Nabi <anabi001@odu.edu>
2024-08-22 23:59:11 -0400

    remove unused imports

--- src/main/java/com/example/stage4/Controller/AuthController.java
+++ src/main/java/com/example/stage4/Controller/AuthController.java
@@ -1,15 +1,12 @@
 package com.example.stage4.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.security.core.Authentication;
 
-import com.example.stage4.Model.Account;
 import com.example.stage4.Security.TokenService;
 
 @RestController
